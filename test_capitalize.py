3a351a8f8b2a200e06189bf6cbd24b1a72b2f804
Onuchin <alex133@LAPTOP-Q7GG4FAM>
2023-07-13 09:51:12 +0300

    create function and test

--- test_capitalize.py
+++ test_capitalize.py
@@ -2,3 +2,5 @@ from capitalize import capitalize
 
 if capitalize('hello') != 'Hello':
     raise Exception('The function woks incorrect')
+if capitalize('') != '':
+    raise Exception('The function woks incorrect!')
